dec23bfcd9108427908574d0d4a45950ca73b6b7
Mohamed Fathy <Mohamed.fathy12111999@gmail.com>
2026-08-08 22:03:56 +0300

    Essential Backend Skills - Exceptions, Validation, Auditing & Bean Scopes

---
+++ section8/jobportal/src/main/java/com/fathy/jobportal/company/service/ICompanyService.java
@@ -0,0 +1,10 @@
+package com.fathy.jobportal.company.service;
+
+import com.fathy.jobportal.dto.CompanyDto;
+
+import java.util.List;
+
+public interface ICompanyService {
+
+    List<CompanyDto> getAllCompanies();
+}
